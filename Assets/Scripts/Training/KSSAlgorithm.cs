87443b1b4fc7bc2e5e2b6bcc2124b4e540ff08b3
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2023-12-01 18:04:15 -0500

    Used System.Serializable instead to remain consistent with the rest of the code

--- Assets/Scripts/Training/KSSAlgorithm.cs
+++ Assets/Scripts/Training/KSSAlgorithm.cs
@@ -7,13 +7,12 @@ using UnityEditor;
 using System.Text;
 using System.Linq;
 using System.IO;
-using System;
 using System.Runtime.Serialization.Formatters.Binary;
 
 
 namespace KSS
 {
-    [Serializable]
+    [System.Serializable]
     public struct GenerationProperty<T>
     {
         public T best;
