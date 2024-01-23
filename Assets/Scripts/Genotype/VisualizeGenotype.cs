405c30fc456cdbb9fe68ee736d02b2a30b5d79aa
Grace Liu <graceliu.gll@gmail.com>
2024-01-23 17:12:49 -0500

    add the graph declerations to neuron visualization (else)

--- Assets/Scripts/Genotype/VisualizeGenotype.cs
+++ Assets/Scripts/Genotype/VisualizeGenotype.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using UnityEngine;
 using System.IO;
@@ -7,8 +8,8 @@ using System.IO;
 [System.Serializable]
 public class VisualizeGenotype
 {
-    //public static string cgToDotString(CreatureGenotype cg, bool visualizeNeurons)
-    public static void cgToDotString(CreatureGenotype cg, bool visualizeNeurons)
+    public static string cgToDotString(CreatureGenotype cg, bool visualizeNeurons)
+    //public static void cgToDotString(CreatureGenotype cg, bool visualizeNeurons)
 
     {
 
@@ -21,7 +22,7 @@ public class VisualizeGenotype
             foreach (SegmentGenotype segment in cg.segments)
             {
 
-                // list all the nodes
+                // declare all the nodes
                 if (segment.id == 1)
                 {
                     dotString += ("1 [label=\"segment 1 (root)\", color=\"#"
@@ -54,43 +55,109 @@ public class VisualizeGenotype
                 }
             }
 
-
-            dotString += "}";
         }
+
         else
         {
-            {}
+            // graph definitions 
+            dotString += "compound = true;"
+                      + "splines = \"polyline\";"
+                      + "rankdir = \"LR\";"
+                      + "1[shape = \"point\"];";
+
+
+            // declare all the subgraphs and nodes 
+
+            foreach (SegmentGenotype segment in cg.segments)
+            {
+
+                // list all the subgraphs
+
+                // code here ....
+
+
+                foreach (NeuronGenotype neuron in segment.neurons)
+                {
+
+                    // list all the neurons within each segment
+
+                    // code here ...
+
+                    string label = neuron.type.ToString();
+                    dotString += "[label=\"" + label + "\"]";
+                    
+
+                }
+
+
+               
+            }
+
+
+
         }
 
-        //return dotString;
-        Debug.Log(dotString);
+
+
+        dotString += "}";
+
+        return dotString;
+        //CreatePngFromDot(dotString, "output.png");
+        //UnityEngine.Debug.Log(dotString);
 
     }
 
+    public static void CreatePngFromDot(string dotString, string outputPath)
+    {
+        // run which dot and replace this string with the output
+        // TODO: Could replace with a new process
+        string dotPath = "/opt/homebrew/bin/dot";
 
-    //public void CreatePngFromDot(string dotString, string outputPath)
-    //{
-    //    // Command to run Graphviz
-    //    string command = "dot";
+        // Create a temporary DOT file
+        UnityEngine.Debug.Log(dotString);
+        string dotFilePath = Path.Combine(Application.persistentDataPath, "temp.dot");
+        File.WriteAllText(dotFilePath, dotString);
 
-    //    // Arguments for the command
-    //    string args = "-Tpng -o \"" + outputPath + "\"";
+        // Start a new process to run the 'dot' command
+        ProcessStartInfo startInfo = new ProcessStartInfo()
+        {
+            // Replace with CMD.exe for Windows, for Mac -> /bin/bash
+            FileName = "/bin/bash",
+            WorkingDirectory = Application.persistentDataPath,
+            UseShellExecute = false,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true,
+            // This is the command we are running in terminal
+            Arguments = "-c \"" + dotPath + " -Tpng \"temp.dot\" -o \"output.png\"" + "\""
+        };
 
-    //    // Start a new process
-    //    ProcessStartInfo startInfo = new ProcessStartInfo(command, args)
-    //    {
-    //        RedirectStandardInput = true,
-    //        UseShellExecute = false
-    //    };
+        Process process = new Process
+        {
+            StartInfo = startInfo
+        };
 
-    //    Process process = Process.Start(startInfo);
+        // Execute our process
+        process.Start();
+        //process.BeginOutputReadLine();
 
-    //    // Write the DOT string to the standard input of the process
-    //    using (StreamWriter writer = process.StandardInput)
-    //    {
-    //        writer.WriteLine(dotString);
-    //    }
+        // This is a sanity check, print our command to the log to check it's correct
+        //UnityEngine.Debug.Log(process.StartInfo.Arguments);
+        process.WaitForExit();
 
-    //    process.WaitForExit();
-    //}
+        // Error Checking
+        if (process.ExitCode != 0)
+        {
+            UnityEngine.Debug.LogError($"Error: 'dot' command failed with exit code {process.ExitCode}");
+        }
+
+
+        // If we run hello world, uncomment these lines to read the standard output
+        //string output = process.StandardOutput.ReadToEnd();
+        //UnityEngine.Debug.Log(output);
+
+        // Clean up: Delete the temporary DOT file
+        File.Delete(dotFilePath);
+
+    }
 }
